07dfc6b83cd02b6d4e6ff453c7c2271acf771ce2
pix <pix666@ya.ru>
2019-06-18 11:23:55 +0400

    Bump to v0.0.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/dldevinc/variations'
 EMAIL = 'x896321475@gmail.com'
 AUTHOR = 'Mihail Mishakin'
 REQUIRES_PYTHON = '>=3.4.0'
-VERSION = '0.0.5'
+VERSION = '0.0.6'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
