5c0239c4b5d401dc630d90e05465ad9d47094b1a
pix666 <pix666@ya.ru>
2021-11-19 16:45:19 +0400

    add pytest option

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,20 @@ from pathlib import Path
 from . import helper
 
 
+def pytest_addoption(parser):
+    parser.addoption("--folder", action="store")
+
+
 def pytest_generate_tests(metafunc):
+    folder_option = metafunc.config.option.folder
+
     if hasattr(metafunc.cls, "input_files"):
         if "input_file" in metafunc.fixturenames:
             argvalues = []
             for dirname in metafunc.cls.input_files:
+                if folder_option is not None and folder_option != dirname:
+                    continue
+
                 resolved_path = (Path(helper.INPUT_PATH) / dirname).resolve()
                 if resolved_path.is_dir():
                     sorted_files = sorted(file for file in resolved_path.iterdir() if file.is_file())
